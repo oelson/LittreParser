3a2ff6e081d7d3520bc21347649a9fb52ce83005
Nelson HOUILLON <houillon.nelson@gmail.com>
2013-11-22 17:18:41 +0100

    formattage texte simple BASIQUE d'une entrée

--- parse.py
+++ parse.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 
-__all__ = ["parser", "entree"]
+__all__ = ["parser", "entry"]
 
 import xml.etree.ElementTree as ET
 
@@ -22,6 +22,12 @@ def _xml2dict(root):
             "children": _xml2dict(child)
         })
     return out
+
+def _gettext(elem):
+    """
+    Équivalent DOM de la propriété "innerText"
+    """
+    return "".join(elem.itertext())
 
 class parser:
     """
@@ -61,10 +67,10 @@ class parser:
             self.load_xml_file(letter)
         return self._ET_parsers[letter]
     
-    def get_entree(self, name):
+    def get_entry(self, name):
         """
         Récupère un noeud Element correspondant au mot recherché.
-        Retourne une instance de la classe "entree".
+        Retourne une instance de la classe "entry".
         """
         # récupère le parseur adéquat
         name = name.upper()
@@ -73,23 +79,90 @@ class parser:
         node = p.find("./entree[@terme='{}']".format(name))
         if node is None:
             raise EntryNotFound("the entry \"{}\" does not exist".format(name))
-        return entree(name, node)
+        return entry(name, node)
 
-class entree:
+
+class entry:
     """
     Une entrée du dictionnaire générée par le parseur.
     Une entrée correspond à une définition.
     """
+    FORMAT_TYPE_PLAINTEXT = 0
+    FORMAT_TYPE_HTML      = 1
+    
+    # Liste à puce
+    _bullet = u"\u00A0\u2219\u00A0"
 
-    def __init__(self, mot, entree):
+    # Format de l'entête
+    entete_format = """terme: {}
+prononciation: {}
+nature: {}"""
+    
+    # Format d'une variante
+    variante_format = _bullet + "variante #{}: {}"
+    
+    
+    def __init__(self, mot, entry):
         self.mot = mot
-        self.entree = entree
+        self.entry = entry
 
-    def format(self):
+    def __dict__(self):
         """
-        Retourne la description de l'objet sus la forme d'un dictionnaire.
+        Retourne la description de l'objet sous la forme d'un dictionnaire.
         """
-        return _xml2dict(self.entree)
+        return _xml2dict(self.entry)
+
+    def format(self, format_type=FORMAT_TYPE_PLAINTEXT):
+        """
+        """
+        if format_type == self.FORMAT_TYPE_PLAINTEXT:
+            return self.format_plaintext()
+        elif format_type == self.FORMAT_TYPE_HTML:
+            return self.format_html()
+        else:
+            raise ValueError
+    
+    def format_plaintext(self):
+        """
+        Les noms de noeuds XML finissent par un '_'.
+        """
+        # Entête de la définition
+        entete_ = self.entry.find("./entete")
+        prononciation_ = entete_.find("./prononciation")
+        nature_ = entete_.find("./nature")
+        entete = self.entete_format.format(
+            self.entry.attrib["terme"],
+            prononciation_.text,
+            nature_.text,
+        )
+        # Corps de la définition
+        corps_ = self.entry.find("./corps")
+        corps = ""
+        f = True
+        for v in corps_.iterfind("./variante"):
+            # Ajoute un saut de ligne entre chaque variante
+            if f:
+                f = False
+            else:
+                corps += "\n"
+            # Texte de la variante
+            id = v.attrib.get("num") or ""
+            corps += self.variante_format.format(
+                id,
+                # premier morceau de texte directement inclus dans le noeud
+                next(v.itertext()).strip()
+            )
+            # Adjoint les éventuelles citations propres à une variante
+            for c in v.iterfind("./cit"):
+                pass
+        # Concaténation des sous-parties0
+        return "{}\n{}".format(
+            entete,
+            corps
+        )
+    
+    def format_html(self):
+        pass
 
     def __repr__(self):
         return self.__str__()
